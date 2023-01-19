a5381ddc55756f27fad78b994cb47fe59f5f8d87
Behnoosh Parsa <behnoosh@amazon.com>
2023-01-19 10:24:46 -0800

    Adjustments for MNIST added to Swin_t. Since the input channel is 1 instead of 3.

--- badge/run.py
+++ badge/run.py
@@ -3,15 +3,15 @@ import sys
 import gc
 import gzip
 import pickle
-#import openml
+# import openml
 import os
 import argparse
 
-from torch.nn import Linear, Sequential
+# from torch.nn import Linear, Sequential
 
 import swin
 from dataset import get_dataset, get_handler
-from model import get_net
+# from model import get_net
 import vgg
 import resnet
 from sklearn.preprocessing import LabelEncoder
@@ -19,17 +19,16 @@ import torch.nn.functional as F
 from torch import nn
 from torchvision import transforms
 import torch
-import time
-import pdb
-from scipy.stats import zscore
-from torchvision.models import Swin_V2_T_Weights, swin_v2_t
+# import time
+# import pdb
+# from scipy.stats import zscore
 from query_strategies import RandomSampling, BadgeSampling, \
-                                BaselineSampling, LeastConfidence, MarginSampling, \
-                                EntropySampling, CoreSet, ActiveLearningByLearning, \
-                                LeastConfidenceDropout, MarginSamplingDropout, EntropySamplingDropout, \
-                                KMeansSampling, KCenterGreedy, BALDDropout, CoreSet, \
-                                AdversarialBIM, AdversarialDeepFool, ActiveLearningByLearning, BaitSampling
+    BaselineSampling, LeastConfidence, MarginSampling, \
+    EntropySampling, ActiveLearningByLearning, BaitSampling, CoreSet  # , , ActiveLearningByLearning, \
 
+# LeastConfidenceDropout, MarginSamplingDropout, EntropySamplingDropout, \
+# KMeansSampling, KCenterGreedy, BALDDropout, CoreSet, \
+# AdversarialBIM, AdversarialDeepFool,
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--alg', help='acquisition algorithm', type=str, default='rand')
@@ -40,7 +39,7 @@ parser.add_argument('--path', help='data path', type=str, default='data')
 parser.add_argument('--data', help='dataset (non-openML)', type=str, default='')
 parser.add_argument('--nQuery', help='number of points to query in a batch', type=int, default=100)
 parser.add_argument('--nStart', help='number of points to start', type=int, default=100)
-parser.add_argument('--nEnd', help = 'total number of points to query', type=int, default=50000)
+parser.add_argument('--nEnd', help='total number of points to query', type=int, default=50000)
 parser.add_argument('--nEmb', help='number of embedding dims (mlp)', type=int, default=128)
 parser.add_argument('--rounds', help='number of rounds (0 does entire dataset)', type=int, default=0)
 parser.add_argument('--trunc', help='dataset truncation (-1 is no truncation)', type=int, default=-1)
@@ -52,60 +51,63 @@ print(opts, flush=True)
 # parameters
 NUM_INIT_LB = opts.nStart
 NUM_QUERY = opts.nQuery
-NUM_ROUND = int((opts.nEnd - NUM_INIT_LB)/ opts.nQuery)
+NUM_ROUND = int((opts.nEnd - NUM_INIT_LB) / opts.nQuery)
 DATA_NAME = opts.data
 # regularization settings for bait
 opts.lamb = 1
 
-
 # non-openml data defaults
 args_pool = {'MNIST':
-                {'n_epoch': 10, 'transform': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]),
-                 'loader_tr_args':{'batch_size': 64, 'num_workers': 1},
-                 'loader_te_args':{'batch_size': 1000, 'num_workers': 1},
-                 'optimizer_args':{'lr': 0.01, 'momentum': 0.5}},
-            'FashionMNIST':
-                {'n_epoch': 10, 'transform': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]),
-                 'loader_tr_args':{'batch_size': 64, 'num_workers': 1},
-                 'loader_te_args':{'batch_size': 1000, 'num_workers': 1},
-                 'optimizer_args':{'lr': 0.01, 'momentum': 0.5}},
-            'SVHN':
-                {'n_epoch': 20, 'transform': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4377, 0.4438, 0.4728), (0.1980, 0.2010, 0.1970))]),
-                 'loader_tr_args':{'batch_size': 64, 'num_workers': 1},
-                 'loader_te_args':{'batch_size': 1000, 'num_workers': 1},
-                 'optimizer_args':{'lr': 0.01, 'momentum': 0.5}},
-            'CIFAR10':
-                {'n_epoch': 3, 'transform': transforms.Compose([ 
+                 {'n_epoch': 10,
+                  'transform': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]),
+                  'loader_tr_args': {'batch_size': 64, 'num_workers': 1},
+                  'loader_te_args': {'batch_size': 1000, 'num_workers': 1},
+                  'optimizer_args': {'lr': 0.01, 'momentum': 0.5}},
+             'FashionMNIST':
+                 {'n_epoch': 10,
+                  'transform': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]),
+                  'loader_tr_args': {'batch_size': 64, 'num_workers': 1},
+                  'loader_te_args': {'batch_size': 1000, 'num_workers': 1},
+                  'optimizer_args': {'lr': 0.01, 'momentum': 0.5}},
+             'SVHN':
+                 {'n_epoch': 20, 'transform': transforms.Compose(
+                     [transforms.ToTensor(), transforms.Normalize((0.4377, 0.4438, 0.4728), (0.1980, 0.2010, 0.1970))]),
+                  'loader_tr_args': {'batch_size': 64, 'num_workers': 1},
+                  'loader_te_args': {'batch_size': 1000, 'num_workers': 1},
+                  'optimizer_args': {'lr': 0.01, 'momentum': 0.5}},
+             'CIFAR10':
+                 {'n_epoch': 3, 'transform': transforms.Compose([
                      transforms.RandomCrop(32, padding=4),
                      transforms.RandomHorizontalFlip(),
                      transforms.ToTensor(),
                      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))
                  ]),
-                 'loader_tr_args':{'batch_size': 128, 'num_workers': 1},
-                 'loader_te_args':{'batch_size': 1000, 'num_workers': 1},
-                 'optimizer_args':{'lr': 0.05, 'momentum': 0.3},
-                 'transformTest': transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))])}
-                }
+                  'loader_tr_args': {'batch_size': 128, 'num_workers': 1},
+                  'loader_te_args': {'batch_size': 1000, 'num_workers': 1},
+                  'optimizer_args': {'lr': 0.05, 'momentum': 0.3},
+                  'transformTest': transforms.Compose([transforms.ToTensor(),
+                                                       transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                                                            (0.2470, 0.2435, 0.2616))])}
+             }
 opts.nClasses = 10
-if opts.model=='swin_t':
-    args_pool['CIFAR10']['transform']= transforms.Compose([
+if opts.model == 'swin_t':
+    args_pool['CIFAR10']['transform'] = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)), #should I change this to the values for Swin_V2_t? (0.485, 0.456, 0.406), (0.229, 0.224, 0.225)
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+        # should I change this to the values for Swin_V2_t? (0.485, 0.456, 0.406), (0.229, 0.224, 0.225)
         transforms.Resize((256, 256))])
 
-if opts.aug == 0: 
-    args_pool['CIFAR10']['transform'] =  args_pool['CIFAR10']['transformTest'] # remove data augmentation
+if opts.aug == 0:
+    args_pool['CIFAR10']['transform'] = args_pool['CIFAR10']['transformTest']  # remove data augmentation
 args_pool['MNIST']['transformTest'] = args_pool['MNIST']['transform']
 args_pool['FashionMNIST']['transformTest'] = args_pool['FashionMNIST']['transform']
 args_pool['SVHN']['transformTest'] = args_pool['SVHN']['transform']
 
-
 if opts.did == 0: args = args_pool[DATA_NAME]
 if not os.path.exists(opts.path):
     os.makedirs(opts.path)
-
 
 # load openml dataset if did is supplied
 if opts.did > 0:
@@ -120,8 +122,7 @@ if opts.did > 0:
     X = X[inds]
     y = y[inds]
 
-
-    split =int((1. - testSplit) * nSamps)
+    split = int((1. - testSplit) * nSamps)
     while True:
         inds = np.random.permutation(split)
         if len(inds) > 50000: inds = inds[:50000]
@@ -138,13 +139,12 @@ if opts.did > 0:
 
         if len(np.unique(Y_tr)) == opts.nClasses: break
 
-
-    args = {'transform':transforms.Compose([transforms.ToTensor()]),
-            'n_epoch':10,
-            'loader_tr_args':{'batch_size': 128, 'num_workers': 1},
-            'loader_te_args':{'batch_size': 1000, 'num_workers': 1},
-            'optimizer_args':{'lr': 0.01, 'momentum': 0},
-            'transformTest':transforms.Compose([transforms.ToTensor()])}
+    args = {'transform': transforms.Compose([transforms.ToTensor()]),
+            'n_epoch': 10,
+            'loader_tr_args': {'batch_size': 128, 'num_workers': 1},
+            'loader_te_args': {'batch_size': 1000, 'num_workers': 1},
+            'optimizer_args': {'lr': 0.01, 'momentum': 0},
+            'transformTest': transforms.Compose([transforms.ToTensor()])}
     handler = get_handler('other')
 
 # load non-openml dataset
@@ -153,15 +153,14 @@ else:
     opts.dim = np.shape(X_tr)[1:]
     handler = get_handler(opts.data)
 
-
 if opts.trunc != -1:
-   inds = np.random.permutation(len(X_tr))[:opts.trunc]
-   X_tr = X_tr[inds]
-   Y_tr = Y_tr[inds]
-   inds = torch.where(Y_tr < 10)[0]
-   X_tr = X_tr[inds]
-   Y_tr = Y_tr[inds]
-   opts.nClasses = int(max(Y_tr) + 1)
+    inds = np.random.permutation(len(X_tr))[:opts.trunc]
+    X_tr = X_tr[inds]
+    Y_tr = Y_tr[inds]
+    inds = torch.where(Y_tr < 10)[0]
+    X_tr = X_tr[inds]
+    Y_tr = Y_tr[inds]
+    opts.nClasses = int(max(Y_tr) + 1)
 
 args['lr'] = opts.lr
 args['modelType'] = opts.model
@@ -183,18 +182,22 @@ idxs_tmp = np.arange(n_pool)
 np.random.shuffle(idxs_tmp)
 idxs_lb[idxs_tmp[:NUM_INIT_LB]] = True
 
+
 # linear model class
 class linMod(nn.Module):
     def __init__(self, dim=28):
         super(linMod, self).__init__()
         self.dim = dim
         self.lm = nn.Linear(dim, opts.nClasses)
+
     def forward(self, x):
         x = x.view(-1, self.dim)
         out = self.lm(x)
         return out, x
+
     def get_embedding_dim(self):
         return self.dim
+
 
 # mlp model class
 class mlpMod(nn.Module):
@@ -206,16 +209,18 @@ class mlpMod(nn.Module):
         self.lm2 = nn.Linear(embSize, embSize)
         self.linear = nn.Linear(embSize, opts.nClasses, bias=False)
         self.useNonLin = useNonLin
+
     def forward(self, x):
         x = x.view(-1, self.dim)
-        if self.useNonLin: emb = F.relu(self.lm1(x))
-        else: emb = self.lm1(x)
+        if self.useNonLin:
+            emb = F.relu(self.lm1(x))
+        else:
+            emb = self.lm1(x)
         out = self.linear(emb)
         return out, emb
+
     def get_embedding_dim(self):
         return self.embSize
-
-
 
 
 # load specified network
@@ -229,7 +234,7 @@ elif opts.model == 'lin':
     dim = np.prod(list(X_tr.shape[1:]))
     net = linMod(dim=int(dim))
 elif opts.model == 'swin_t':
-    if opts.data =='MNIST':
+    if opts.data == 'MNIST':
         net = swin.MyCustomSwinTiny(input_channel=1)
     else:
         net = swin.MyCustomSwinTiny(input_channel=3, pretrained=True)
@@ -245,32 +250,32 @@ if type(X_tr[0]) is not np.ndarray:
     X_tr = X_tr.numpy()
 
 # set up the specified sampler
-if opts.alg == 'rand': # random sampling
+if opts.alg == 'rand':  # random sampling
     strategy = RandomSampling(X_tr, Y_tr, idxs_lb, net, handler, args)
-elif opts.alg == 'bait': # bait sampling
+elif opts.alg == 'bait':  # bait sampling
     strategy = BaitSampling(X_tr, Y_tr, idxs_lb, net, handler, args)
-elif opts.alg == 'conf': # confidence-based sampling
+elif opts.alg == 'conf':  # confidence-based sampling
     strategy = LeastConfidence(X_tr, Y_tr, idxs_lb, net, handler, args)
-elif opts.alg == 'marg': # margin-based sampling
+elif opts.alg == 'marg':  # margin-based sampling
     strategy = MarginSampling(X_tr, Y_tr, idxs_lb, net, handler, args)
-elif opts.alg == 'badge': # batch active learning by diverse gradient embeddings
+elif opts.alg == 'badge':  # batch active learning by diverse gradient embeddings
     strategy = BadgeSampling(X_tr, Y_tr, idxs_lb, net, handler, args)
-elif opts.alg == 'coreset': # coreset sampling
+elif opts.alg == 'coreset':  # coreset sampling
     strategy = CoreSet(X_tr, Y_tr, idxs_lb, net, handler, args)
-elif opts.alg == 'entropy': # entropy-based sampling
+elif opts.alg == 'entropy':  # entropy-based sampling
     strategy = EntropySampling(X_tr, Y_tr, idxs_lb, net, handler, args)
-elif opts.alg == 'baseline': # badge but with k-DPP sampling instead of k-means++
+elif opts.alg == 'baseline':  # badge but with k-DPP sampling instead of k-means++
     strategy = BaselineSampling(X_tr, Y_tr, idxs_lb, net, handler, args)
-elif opts.alg == 'albl': # active learning by learning
+elif opts.alg == 'albl':  # active learning by learning
     albl_list = [LeastConfidence(X_tr, Y_tr, idxs_lb, net, handler, args),
-        CoreSet(X_tr, Y_tr, idxs_lb, net, handler, args)]
+                 CoreSet(X_tr, Y_tr, idxs_lb, net, handler, args)]
     strategy = ActiveLearningByLearning(X_tr, Y_tr, idxs_lb, net, handler, args, strategy_list=albl_list, delta=0.1)
-else: 
+else:
     print('choose a valid acquisition function', flush=True)
     raise ValueError
 
 # print info
-if opts.did > 0: DATA_NAME='OML' + str(opts.did)
+if opts.did > 0: DATA_NAME = 'OML' + str(opts.did)
 print(DATA_NAME, flush=True)
 print(type(strategy).__name__, flush=True)
 
@@ -279,11 +284,11 @@ if type(X_te) == torch.Tensor: X_te = X_te.numpy()
 # round 0 accuracy
 strategy.train()
 P = strategy.predict(X_te, Y_te)
-acc = np.zeros(NUM_ROUND+1)
+acc = np.zeros(NUM_ROUND + 1)
 acc[0] = 1.0 * (Y_te == P).sum().item() / len(Y_te)
 print(str(opts.nStart) + '\ttesting accuracy {}'.format(acc[0]), flush=True)
 
-for rd in range(1, NUM_ROUND+1):
+for rd in range(1, NUM_ROUND + 1):
     print('Round {}'.format(rd), flush=True)
     torch.cuda.empty_cache()
     gc.collect()
@@ -305,4 +310,3 @@ for rd in range(1, NUM_ROUND+1):
     print(str(sum(idxs_lb)) + '\t' + 'testing accuracy {}'.format(acc[rd]), flush=True)
     if sum(~strategy.idxs_lb) < opts.nQuery: break
     if opts.rounds > 0 and rd == (opts.rounds - 1): break
-
