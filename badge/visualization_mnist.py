dd6d82d5def81215517677ecf2cce148c34e1a68
Behnoosh Parsa <behnoosh@amazon.com>
2023-02-10 14:53:24 -0800

    HypUmapSampleing done

--- badge/visualization_mnist.py
+++ badge/visualization_mnist.py
@@ -41,7 +41,8 @@ plt.show()
 fig.savefig('MNIST_Swin-t_10000.png')
 
 
-#%% MNIST batch size=1000, Rand and Badge comparison
+
+#%% MNIST batch size=100, Rand and Badge comparison
 
 batch_size_swin_t_MNIST = []
 test_accuracy_swin_t_MNIST = []
@@ -57,6 +58,20 @@ with open ('swin_t_MNIST_1000_badge_log.txt', 'rt') as myfile:  # Open lorem.txt
                 test_accuracy_swin_t_MNIST.append(float(myline.split(' ')[-1]))
                 print(int(myline.split(' ')[0][0:5]), float(myline.split(' ')[-1]))                  # and print the string.
 
+
+batch_size_hypUMAP_MNIST = []
+test_accuracy_hypUMAP_MNIST = []
+with open ('swin_t_MNIST_1000_hypUMAP_log.txt', 'rt') as myfile:  # Open lorem.txt for reading
+    for myline in myfile:              # For each line, read to a string,
+        if 'testing accuracy' in myline:
+            if 't'in myline.split(' ')[0][0:5]:
+                batch_size_hypUMAP_MNIST.append(int(myline.split(' ')[0][0:3]))
+                test_accuracy_hypUMAP_MNIST.append(float(myline.split(' ')[-1]))
+                print(int(myline.split(' ')[0][0:3]), float(myline.split(' ')[-1]))
+            else:
+                batch_size_hypUMAP_MNIST.append(int(myline.split(' ')[0][0:5]))
+                test_accuracy_hypUMAP_MNIST.append(float(myline.split(' ')[-1]))
+                print(int(myline.split(' ')[0][0:5]), float(myline.split(' ')[-1]))
 
 batch_size_rand_MNIST = []
 test_accuracy_rand_MNIST = []
@@ -76,14 +91,16 @@ with open ('swin_t_MNIST_1000_Rand_log.txt', 'rt') as myfile:  # Open lorem.txt 
 fig = plt.figure()
 plt.plot(batch_size_swin_t_MNIST, test_accuracy_swin_t_MNIST, label='badge')
 plt.plot(batch_size_rand_MNIST, test_accuracy_rand_MNIST, label='Rand')
+plt.plot(batch_size_hypUMAP_MNIST, test_accuracy_hypUMAP_MNIST, label='HypUmap')
 plt.title("MNIST Swin-t 1000")
-plt.ylim([0,1.0])
+plt.ylim([0.5,1.0])
 plt.legend()
 plt.show()
-fig.savefig('MNIST_Swin-t_1000.png')
+fig.savefig('MNIST_Swin-t_1000_2.png')
 
 
-#%% MNIST batch size=1000, Rand and Badge comparison
+
+#%% MNIST batch size=100, Rand and Badge comparison
 
 batch_size_swin_t_MNIST = []
 test_accuracy_swin_t_MNIST = []
