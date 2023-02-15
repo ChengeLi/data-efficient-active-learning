0daecf98af8ab7103ed45bc341dae4e84b8d8283
Behnoosh Parsa <behnoosh@amazon.com>
2023-02-15 11:03:50 -0800

    Updated saving embedding to enable later demo building

--- badge/query_strategies/hyperbolic_embedding_umap_sampling.py
+++ badge/query_strategies/hyperbolic_embedding_umap_sampling.py
@@ -138,13 +138,13 @@ class HypUmapSampleing(Strategy):
         if len(os.listdir(self.output_image_dir)) != 0:
             name = int(sorted(os.listdir(self.output_image_dir))[-1][:-4])+1
             image_name = os.path.join(self.output_image_dir,"{:05d}.png".format(name))
-            selected_sample_name = os.path.join(self.output_sample_dir,"chosen_{:05d}.txt".format(name))
-            all_sample_name = os.path.join(self.output_sample_dir,"all_{:05d}.txt".format(name))
+            selected_sample_name = os.path.join(self.output_sample_dir,"chosen_{:05d}.csv".format(name))
+            all_sample_name = os.path.join(self.output_sample_dir,"all_{:05d}.csv".format(name))
             del name
         else:
             image_name = os.path.join(self.output_image_dir,'00000.png')
-            selected_sample_name = os.path.join(self.output_sample_dir,'chosen_00000.txt')
-            all_sample_name = os.path.join(self.output_sample_dir,'all_00000.txt')
+            selected_sample_name = os.path.join(self.output_sample_dir,'chosen_00000.csv')
+            all_sample_name = os.path.join(self.output_sample_dir,'all_00000.csv')
         # Get embedding for all data
         print('Get embedding for all Samples ...')
         embedding = self.get_embedding(self.X, self.Y)
@@ -155,37 +155,44 @@ class HypUmapSampleing(Strategy):
         # hyperbolic_mapper = umap.UMAP(n_components=10,n_neighbors=5,output_metric='hyperboloid',metric='minkowski',
         #                               random_state=42,tqdm_kwds={'disable':False}).fit(hypEmbedding)
         print('Training UMAP on all samples and returning the embedding in Euclidean space ...')
-        hyperbolic_to_euclidean_emb = umap.UMAP(n_components=10, output_metric='euclidean', metric='minkowski',
-                                                random_state=42, tqdm_kwds={'disable': False}).fit_transform(hypEmbedding)
+        all_emb = umap.UMAP(n_components=10, output_metric='euclidean', metric='minkowski',
+                            random_state=42, tqdm_kwds={'disable': False}).fit_transform(hypEmbedding)
         print('Normalize embedding in Euclidean space ...')
-        hyperbolic_to_euclidean_emb = hyperbolic_to_euclidean_emb / max(scipy.linalg.norm(hyperbolic_to_euclidean_emb, axis=1))
-        np.savetxt(all_sample_name, hyperbolic_to_euclidean_emb)
-
+        all_emb = all_emb / max(scipy.linalg.norm(all_emb, axis=1))
+        header_ = ['emb_' + str(i) for i in range(np.shape(all_emb)[1])]
+        header_ = ['label'] + header_
+        df = pd.DataFrame(np.concatenate([np.expand_dims((self.Y).numpy(),axis=1), all_emb],axis=1), columns=header_)
+        df.to_csv(all_sample_name,index=False)
         # compute hyp-emb for all unlabeled
         idxs_unlabeled = np.arange(self.n_pool)[~self.idxs_lb]
 
         # Run Kmeans to get clusters and sample association
         print('Running Kmean++ in Euclidean space ...')
-        chosen = init_centers(hyperbolic_to_euclidean_emb[idxs_unlabeled], n)
-        hyperbolic_to_euclidean_emb_chosen = hyperbolic_to_euclidean_emb[idxs_unlabeled[chosen]]
-        np.savetxt(selected_sample_name, np.concatenate([np.expand_dims(idxs_unlabeled[chosen],axis=1),hyperbolic_to_euclidean_emb_chosen], axis=1))
-        plt.scatter(hyperbolic_to_euclidean_emb.T[0],
-                    hyperbolic_to_euclidean_emb.T[1],
+        chosen = init_centers(all_emb[idxs_unlabeled], n)
+        chosen_emb = all_emb[idxs_unlabeled[chosen]]
+        header_ = ['emb_' + str(i) for i in range(np.shape(chosen_emb)[1])]
+        header_ = ['label', 'index'] + header_
+        df = pd.DataFrame(np.concatenate(
+            [np.expand_dims((self.Y[chosen]).numpy(), axis=1), np.expand_dims(idxs_unlabeled[chosen], axis=1),
+             chosen_emb], axis=1), columns=header_)
+        df.to_csv(selected_sample_name, index=False)
+        plt.scatter(all_emb.T[0],
+                    all_emb.T[1],
                     c=self.Y, cmap='Spectral')
-        plt.scatter(hyperbolic_to_euclidean_emb_chosen.T[0],
-                    hyperbolic_to_euclidean_emb_chosen.T[1],
+        plt.scatter(chosen_emb.T[0],
+                    chosen_emb.T[1],
                     c=self.Y[idxs_unlabeled[chosen]],
                     edgecolor='black', linewidth=0.3, marker='*', cmap='Spectral')
         plt.xlim([-1,1])
         plt.ylim([-1,1])
         # emb = np.concatenate(
-        #     [np.expand_dims(np.arange(0, len(hyperbolic_to_euclidean_emb)), axis=1), hyperbolic_to_euclidean_emb],
+        #     [np.expand_dims(np.arange(0, len(all_emb)), axis=1), all_emb],
         #     axis=1)[:, 0:3]
         # emb = pd.DataFrame(emb, columns=['node', 'x', 'y'])
-        # plot_clusters_no_edge(emb, self.Y, hyperbolic_to_euclidean_emb_chosen[:,0:2], self.classes)
+        # plot_clusters_no_edge(emb, self.Y, chosen_emb[:,0:2], self.classes)
         plt.savefig(image_name)
         plt.close('all')
-        del hyperbolic_to_euclidean_emb_chosen, hyperbolic_to_euclidean_emb
+        del chosen_emb, all_emb, df
         return idxs_unlabeled[chosen]
 
 
