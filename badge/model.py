fix grad in MLR, add Riemannian gradient based badge, add Net00 as baseline model, add Sampling based on hyperbolic embedding norm

badge/model.py
@@ -17,7 +17,7 @@ class HyperNet(nn.Module):
     # https://github.com/leymir/hyperbolic-image-embeddings/blob/master/examples/mnist.py
     def __init__(self):
         ## hyperparameters
-        dim = 2 #"Dimension of the Poincare ball"
+        self.poincare_ball_dim = 20 #"Dimension of the Poincare ball"
         c = 1.0 #"Curvature of the Poincare ball"
         train_x = False # train the exponential map origin
         train_c = False # train the Poincare ball curvature
@@ -26,11 +26,12 @@ class HyperNet(nn.Module):
         self.conv1 = nn.Conv2d(1, 20, 5, 1)
         self.conv2 = nn.Conv2d(20, 50, 5, 1)
         self.fc1 = nn.Linear(4 * 4 * 50, 500)
-        self.fc2 = nn.Linear(500, dim)
+        self.fc2 = nn.Linear(500, self.poincare_ball_dim)
         self.tp = hypnn.ToPoincare(
-            c=c, train_x=train_x, train_c=train_c, ball_dim=dim
+            c=c, train_x=train_x, train_c=train_c, ball_dim=self.poincare_ball_dim
         )
-        self.mlr = hypnn.HyperbolicMLR(ball_dim=dim, n_classes=10, c=c)
+        self.mlr = hypnn.HyperbolicMLR(ball_dim=self.poincare_ball_dim, n_classes=10, c=c)
+        # self.mlr = hypnn.HyperbolicMLR_fix_grad(ball_dim=dim, n_classes=10, c=c)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -46,7 +47,7 @@ class HyperNet(nn.Module):
 
     def get_embedding_dim(self):
         # return 500 #if use after fc1 as embedding
-        return 2 # if use after fc2 as embedding
+        return self.poincare_ball_dim # if use after fc2 as embedding
 
 
 class Net0(nn.Module):
@@ -73,6 +74,37 @@ class Net0(nn.Module):
 
     def get_embedding_dim(self):
         return 500
+
+
+class Net00(nn.Module):
+    ### This is hyperNet without the last layer
+    def __init__(self):
+        super(Net00, self).__init__()
+        dim = 10
+
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4 * 4 * 50, 500)
+        self.fc2 = nn.Linear(500, 2) ### mimic poincare ball
+        self.fc3 = nn.Linear(2, dim)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4 * 4 * 50)
+        e1 = F.relu(self.fc1(x))
+        x = F.dropout(e1, training=self.training)
+        e2 = F.relu(self.fc2(x))
+        x = F.dropout(e2, training=self.training)
+        x = self.fc3(x)
+        return x, e2
+
+    def get_embedding_dim(self):
+        return 2
+
+
 
 
 

badge/query_strategies/hyperbolic_embedding_umap_sampling.py
@@ -59,6 +59,7 @@ class PoincareKmeansSampling(Strategy):
             indsAll.append(ind)
             cent += 1
         return indsAll
+    
     def query(self, n):
 
         if len(os.listdir(self.output_sample_dir)) != 0:
@@ -201,6 +202,7 @@ class UmapPoincareKmeansSampling(Strategy):
             indsAll.append(ind)
             cent += 1
         return indsAll
+    
     def query(self, n):
 
         if len(os.listdir(self.output_image_dir)) != 0:
@@ -627,7 +629,7 @@ class HypNetBadgeSampling(Strategy):
     def __init__(self, X, Y, idxs_lb, net, handler, args):
         super(HypNetBadgeSampling, self).__init__(X, Y, idxs_lb, net, handler, args)
 
-    def init_centers(self,X, K):
+    def init_centers(self, X, K):
         ind = np.argmax([np.linalg.norm(s, 2) for s in X])  # this only make sense for badge
         mu = [X[ind]]
         indsAll = [ind]
@@ -654,16 +656,41 @@ class HypNetBadgeSampling(Strategy):
             indsAll.append(ind)
             cent += 1
         return indsAll
+
+
     def query(self, n):
-        # compute hyp-emb for all unlabeled
-        # get_grad
-        # Run Kmeans to get clusters and sample association
-        # chosen = init_centers(gradEmbedding, n)
+        """
+            option 1: use regular gradient in badge
+            option 2: fix grad using riemannian gradient in badge
+            
+        """
+        use_Riemannian_grad_badge = True
 
         idxs_unlabeled = np.arange(self.n_pool)[~self.idxs_lb]
-        gradEmbedding = self.get_grad_embedding_for_hyperNet(self.X[idxs_unlabeled], self.Y.numpy()[idxs_unlabeled]).numpy()
+        gradEmbedding = self.get_grad_embedding_for_hyperNet(self.X[idxs_unlabeled], self.Y.numpy()[idxs_unlabeled], 
+                                                             fix_grad=use_Riemannian_grad_badge).numpy()
         chosen = self.init_centers(gradEmbedding, n)
         return idxs_unlabeled[chosen]
 
+
+
+class HypNetNormSampling(Strategy):
+    """
+        use hyperbolic embedding's norm as a measure of uncertainty
+    """
+    def __init__(self, X, Y, idxs_lb, net, handler, args):
+        super(HypNetNormSampling, self).__init__(X, Y, idxs_lb, net, handler, args)
+
+    def init_centers(self, X, K):
+        hyper_emb_norm = [np.linalg.norm(s, 2) for s in X]
+        indsAll = np.argsort(hyper_emb_norm, axis=0)[:K] #select the smallest K samples
+        return indsAll
+
+
+    def query(self, n):
+        idxs_unlabeled = np.arange(self.n_pool)[~self.idxs_lb]
+        gradEmbedding = self.get_hyperbolic_embedding_norm(self.X[idxs_unlabeled], self.Y.numpy()[idxs_unlabeled]).numpy()
+        chosen = self.init_centers(gradEmbedding, n)
+        return idxs_unlabeled[chosen]
 
 

badge/run.py
@@ -13,10 +13,10 @@ import swin
 from query_strategies.util import create_directory
 from query_strategies.hyperbolic_embedding_umap_sampling import HypUmapSampling, HypNetBadgeSampling, \
     UmapPoincareKmeansSampling, UmapHyperboloidKmeansSampling, UmapHyperboloidKmeansSampling2, \
-    HyperboloidKmeansSampling, PoincareKmeansSampling
+    HyperboloidKmeansSampling, PoincareKmeansSampling, HypNetNormSampling
 from dataset import get_dataset, get_handler
 # from model import get_net
-from model import HyperNet, Net0
+from model import HyperNet, Net0, Net00
 import vgg
 import resnet
 from sklearn.preprocessing import LabelEncoder
@@ -234,7 +234,7 @@ class mlpMod(nn.Module):
         return self.embSize
 
 
-EXPERIMENT_NAME = DATA_NAME + '_' + opts.model + '_' + opts.alg + '_' + str(NUM_QUERY)
+EXPERIMENT_NAME = DATA_NAME + '_' + opts.model + opts.alg + '_' + str(NUM_QUERY) +'_balldim20'
 args['output_dir'] = os.path.join('./badge/output', EXPERIMENT_NAME)
 create_directory(args['output_dir'])
 # load specified network
@@ -258,6 +258,9 @@ elif opts.model == 'HyperNet':
 elif opts.model == 'net0':
     print('Using Net0')
     net = Net0()
+elif opts.model == 'net00':
+    print('Using Net00')
+    net = Net00()
 else:
     print('choose a valid model - mlp, resnet, or vgg', flush=True)
     raise ValueError
@@ -294,6 +297,8 @@ elif opts.alg == 'UmapHyperboloidKmeans2':
     strategy = UmapHyperboloidKmeansSampling2(X_tr, Y_tr, idxs_lb, net, handler, args)
 elif opts.alg == 'hypNetBadge':
     strategy = HypNetBadgeSampling(X_tr, Y_tr, idxs_lb, net, handler, args)
+elif opts.alg == 'hypNetNorm':
+    strategy = HypNetNormSampling(X_tr, Y_tr, idxs_lb, net, handler, args)
 elif opts.alg == 'coreset':  # coreset sampling
     strategy = CoreSet(X_tr, Y_tr, idxs_lb, net, handler, args)
 elif opts.alg == 'entropy':  # entropy-based sampling
@@ -354,32 +359,38 @@ np.savetxt(os.path.join(args['output_dir'],EXPERIMENT_NAME+'_strategy_performanc
 
 if visualize_embedding:
     import cv2
-    import numpy as np
 
     images_dir = os.path.join(args['output_dir'],'images')
-    img_array = []
-    for filename in sorted(os.listdir(images_dir)):
-        img = cv2.imread(os.path.join(images_dir, filename))
-        height, width, layers = img.shape
-        size = (width, height)
-        img_array.append(img)
+    if not os.path.exists(args['output_dir']):
+        create_directory(args['output_dir'])
+    if not os.path.exists(images_dir):
+        create_directory(images_dir)
+    if len(os.listdir(images_dir))>0:
+        img_array = []
+        for filename in sorted(os.listdir(images_dir)):
+            img = cv2.imread(os.path.join(images_dir, filename))
+            height, width, layers = img.shape
+            size = (width, height)
+            img_array.append(img)
 
-    out = cv2.VideoWriter(os.path.join(args['output_dir'],EXPERIMENT_NAME+'_demo_emb.mp4'), cv2.VideoWriter_fourcc(*'MP4V'), 3, size)
+        out = cv2.VideoWriter(os.path.join(args['output_dir'],EXPERIMENT_NAME+'_demo_emb.mp4'), cv2.VideoWriter_fourcc(*'MP4V'), 3, size)
 
-    for i in range(len(img_array)):
-        out.write(img_array[i])
-    out.release()
+        for i in range(len(img_array)):
+            out.write(img_array[i])
+        out.release()
 
 if visualize_learningcurve:
     import matplotlib.pyplot as plt
     results = np.loadtxt(os.path.join(args['output_dir'],EXPERIMENT_NAME+'_strategy_performance.txt'))
     fig = plt.figure()
     plt.plot(results[:,0], results[:,1], label=opts.alg)
+    plt.scatter(results[:,0], results[:,1])
     plt.xlabel('Samples')
     plt.ylabel('Accuracy')
     plt.title(EXPERIMENT_NAME)
     plt.ylim([0.5, 1.0])
     plt.legend()
+    plt.grid('on')
     plt.show()
     fig.savefig(os.path.join(args['output_dir'],EXPERIMENT_NAME + '_learning_curve.png'))
 